905ae32fa32aa2de629095b1de20b3334b1e43ec
HarshThale <thaleharsh48@gmail.com>
2023-09-05 15:45:57 +0530

    commit:10

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,3 +6,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=100)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
+    
