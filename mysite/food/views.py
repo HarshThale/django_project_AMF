2f1ab73f2924f2db89d023a22ddd105220206fd5
HarshThale <thaleharsh48@gmail.com>
2023-09-06 16:06:58 +0530

    commit:13

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -7,11 +7,11 @@ from food.models import Item
 def index(request):
     itemlist = Item.objects.all()
     
-    context = {
-        'itemlist':itemlist
+    context = {              #dictionary
+        'itemlist':itemlist   #'itemlist': -->key
     }
     
-    return render(request, 'food/index.html', context)
+    return render(request, 'food/index.html', context)  #render the request food/index
     
 def detail(request):
     return HttpResponse('this is a detail page')
