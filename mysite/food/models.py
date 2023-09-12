2e6edf6ddc7045c0a616505e68f683daebee63cc
HarshThale <thaleharsh48@gmail.com>
2023-09-12 16:38:14 +0530

    commit:26

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,7 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=100)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500,
+        default="https://kwalityindiancuisine.com/assets/img/food_images/placeholder.png"
+    )
 
     def __str__(self):
         return self.item_name
-    
