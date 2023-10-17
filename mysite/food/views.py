ea7022568d28f0957bf7518e43ac39067bd6c468
HarshThale <thaleharsh48@gmail.com>
2023-10-17 16:16:47 +0530

    commit:71

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -6,6 +6,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from food.models import History
 
 # Create your views here.
 #---------------------------------------------------------------------------------------------------------
@@ -96,6 +97,16 @@ class CreateItem(CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        
+        obj_History = History(
+            user_name = self.request.user.username,
+            prod_ref = form.instance.prod_code,
+            item_name = self.request.POST.get('item_name'), #form.instance.item_name
+            op_type = 'Created'
+        )
+        
+        obj_History.save()
+        
         return super().form_valid(form)
 
 
